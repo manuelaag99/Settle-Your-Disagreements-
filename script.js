91e3cafd8e4968bb24de0578c9f40c19467b7128
Manuel Alanis <manuelaag99@gmail.com>
2022-12-17 19:36:39 -0600

    2022-12-17 Last C

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-document.querySelector("main-container items-container dice-container img").innerHTML 
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var imgName1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("div.main-container img")[0].setAttribute("src", imgName1);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var imgName2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("div.main-container img")[1].setAttribute("src", imgName2);
+
+if (randomNumber1 === randomNumber2) {
+    titleText = "It's a tie! Refresh!"
+} else if (randomNumber1 < randomNumber2) {
+    titleText = "Player 2 wins!"
+} else if (randomNumber1 > randomNumber2) {
+    titleText = "Player 1 wins!"
+}
+
+document.querySelector("h1").innerText = titleText
+
+console.log(titleText)
+
+//document.querySelectorAll("div.main-container img").setAttribute("src", imgName);
+//document.querySelectorAll("div.main-container img")[1].setAttribute("src", imgName);
